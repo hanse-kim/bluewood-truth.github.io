3e7ef32813a53e867b92804bfa721a3c333ee5ce
bluewood <bluewood.truth@gmail.com>
2022-09-06 00:01:14 +0900

    rework post list animation

--- src/views/postList/styled.ts
+++ src/views/postList/styled.ts
@@ -6,13 +6,61 @@ const PostList = styled.ul`
   flex-direction: column;
   row-gap: 36px;
   margin-bottom: 72px;
-  animation: ${fadeIn} 0.5s ease;
 `;
 
 const PostItem = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: 12px;
+  opacity: 0;
+  animation: ${fadeIn} 0.5s ease;
+  animation-fill-mode: forwards;
+
+  &:nth-child(1) {
+    animation-delay: 0.1s;
+  }
+  &:nth-child(2) {
+    animation-delay: 0.2s;
+  }
+  &:nth-child(3) {
+    animation-delay: 0.3s;
+  }
+  &:nth-child(4) {
+    animation-delay: 0.4s;
+  }
+  &:nth-child(5) {
+    animation-delay: 0.5s;
+  }
+  &:nth-child(6) {
+    animation-delay: 0.6s;
+  }
+  &:nth-child(7) {
+    animation-delay: 0.7s;
+  }
+  &:nth-child(8) {
+    animation-delay: 0.8s;
+  }
+  &:nth-child(9) {
+    animation-delay: 0.9s;
+  }
+  &:nth-child(10) {
+    animation-delay: 1s;
+  }
+  &:nth-child(11) {
+    animation-delay: 1.1s;
+  }
+  &:nth-child(12) {
+    animation-delay: 1.2s;
+  }
+  &:nth-child(13) {
+    animation-delay: 1.3s;
+  }
+  &:nth-child(14) {
+    animation-delay: 1.4s;
+  }
+  &:nth-child(15) {
+    animation-delay: 1.5s;
+  }
 `;
 
 const PostItemHeader = styled.hgroup`
